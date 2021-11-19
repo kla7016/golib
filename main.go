7336079aa18f06f850c0e75c69c95662ef7427a4
Sirawit Chanprapap <sirawit.ch@inet.co.th>
2021-11-19 19:20:00 +0700

    add go lib

--- main.go
+++ main.go
@@ -2,21 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/kla7016/golib/sftp"
-	"log"
 )
 
 func main() {
-	client, err := sftp.NewConn("203.151.254.199", "sftpuser", "SfTp@Passw0rd#123", 22)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	listFile, err := client.ListFile("/sftpuser/moderna-data")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	for _, file := range listFile {
-		fmt.Println("name", file.Name, "size", file.Size, "mod_time", file.ModTime)
-	}
+	fmt.Println("go lib")
 }
